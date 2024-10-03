4681906092f7fb859b30a3966a3e32127947b60e
suman492 patro <suman.pat49@gmail.com>
2024-10-03 05:09:48 +0000

    update by id added

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -42,3 +42,20 @@ exports.getstudentById = async(req, res) => {
         res.status(400).send(err.message);
     }
 };
+
+exports.updateStudent = async(req, res) => {
+    try{
+        const studentById = await studentModel.findByIdAndUpdate(req.params.id, {first_name: req.body.first_name,
+            last_name : req.body.last_name,
+            mail_id: req.body.mail_id,
+            current_address: req.body.current_address,
+            attendence: req.body.attendence,
+            total_score: req.body.total_score,
+            avg_cgpa: req.body.avg_cgpa}, { new: true });
+        if(!studentById) return res.status(404).send('Book not found in database');
+        res.send(studentById);
+        console.log("Student updated successfully");
+    } catch(err){
+        res.status(400).send(err.message);
+    }
+};
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -6,8 +6,8 @@ const studentController = require('../controllers/studentController.js');
 
 router.post('/student', studentController.createStudentInfo);      // Create a newSTUDENT
 router.get('/student', studentController.getstudentInfo);
-router.get('student/:id',studentController.getstudentById);
-
+router.get('/student/:id',studentController.getstudentById);
+router.put('/student/:id',studentController.updateStudent);
 
 
 module.exports = router;
