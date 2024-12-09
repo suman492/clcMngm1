9bc853f1fe906eb14f0c19db474052caacb63c28
suman492 patro <suman.pat49@gmail.com>
2024-12-09 09:38:17 +0000

    updated createstudent

--- client/src/components/CreateStudent.js
+++ client/src/components/CreateStudent.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 const CreateStudent = (props) => {
     const navigate = useNavigate();
-    const [student, setStudent ] = useState({
+    const [student, setStudent] = useState({
         name: '',
         mail_id: '',
         dob: '',
@@ -18,145 +18,145 @@ const CreateStudent = (props) => {
 
     const onChange = (e) => {
         console.log(e.target.value)
-        setStudent({...student,[e.target.name]: e.target.value})
+        setStudent({ ...student, [e.target.name]: e.target.value })
         // setStudent({ ...student, [e.target.mail_id]: e.target.value });
-      };
-      console.log(student)
+    };
+    console.log(student)
 
-      const onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-    
-        axios
-          .post('/api/students', student)
-          .then((res) => {
-            setStudent({
-                name: '',
-                mail_id: '',
-                dob: '',
-                current_address: '',
-                attendence: '',
-                total_score: '',
-                avg_cgpa: ''
-            });
 
-            // Delay the navigation slightly to allow the toast to be seen
-        setTimeout(() => {
-            //setShowToast(false); // Hide the toast
-            navigate('/'); // Navigate to homepage
-          }, 5000); // Adjust the timeout as needed
-  
-        })
-        .catch((err) => {
-            console.log('Error in CreateStudent!');
-            console.log('The error is ->')
-            console.log(err)
-        });
-      };
+        axios
+            .post('/api/students', student)
+            .then((res) => {
+                setStudent({
+                    name: '',
+                    mail_id: '',
+                    dob: '',
+                    current_address: '',
+                    attendence: '',
+                    total_score: '',
+                    avg_cgpa: ''
+                });
+
+                // Delay the navigation slightly to allow the toast to be seen
+                setTimeout(() => {
+                    //setShowToast(false); // Hide the toast
+                    navigate('/'); // Navigate to homepage
+                }, 5000); // Adjust the timeout as needed
+
+            })
+            .catch((err) => {
+                console.log('Error in CreateStudent!');
+                console.log('The error is ->')
+                console.log(err)
+            });
+    };
 
 
     return (
-        
-        <div className='CreateStudent'>
-    {/* <Navbar /> */}
-    <div className='container d-flex align-items-center justify-content-center '>
-        <div className='row w-100'>
-            <div className='col-md-8 m-auto'>
-                <br />
-                <Link to='/create-student' className='btn btn-outline-warning float-left'>
-                    Show Student list
-                </Link>
-            </div>
-            <div className='col-md-8 m-auto'  style={{display:"flex",alignItems:"center",flexDirection:"column"}}>
-                <h1 className='display-4 text-center'>Add Student</h1>
-                <p className='lead text-center'>Create new student</p>
 
-                <form noValidate onSubmit={onSubmit}>
-                    <div className='form-group'>
-                        <input
-                            type='text'
-                            placeholder='Name of the Student'
-                            name='name'
-                            className='form-control'
-                            value={student.name}
-                            onChange={onChange}
-                        />
+        <div className='CreateStudent'>
+            {/* <Navbar /> */}
+            <div className='container d-flex align-items-center justify-content-center '>
+                <div className='row w-100'>
+                    <div className='col-md-8 m-auto'>
+                        <br />
+                        <Link to='/create-student' className='btn btn-outline-warning float-left'>
+                            Show Student list
+                        </Link>
                     </div>
-                    <br />
-                    <div className='form-group'>
-                        <input
-                            type='email'
-                            placeholder='Email'
-                            name='email'
-                            className='form-control'
-                            value={student.email}
-                            onChange={onChange}
-                        />
+                    <div className='col-md-8 m-auto' style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+                        <h1 className='display-4 text-center'>Add Student</h1>
+                        <p className='lead text-center'>Create new student</p>
+
+                        <form noValidate onSubmit={onSubmit}>
+                            <div className='form-group'>
+                                <input
+                                    type='text'
+                                    placeholder='Name of the Student'
+                                    name='name'
+                                    className='form-control'
+                                    value={student.name}
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <br />
+                            <div className='form-group'>
+                                <input
+                                    type='email'
+                                    placeholder='Email'
+                                    name='mail_id'
+                                    className='form-control'
+                                    value={student.mail_id}
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <br />
+                            <div className='form-group'>
+                                <input
+                                    type='date'
+                                    placeholder='Enter date of birth'
+                                    name='dob'
+                                    className='form-control'
+                                    value={student.dob}
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <br />
+                            <div className='form-group'>
+                                <input
+                                    type='text'
+                                    placeholder='Enter your current address'
+                                    name='current_address'
+                                    className='form-control'
+                                    value={student.current_address}
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <br />
+                            <div className='form-group'>
+                                <input
+                                    type='text'
+                                    placeholder='Enter True or False'
+                                    name='attendence'
+                                    className='form-control'
+                                    value={student.attendence}
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <br />
+                            <div className='form-group'>
+                                <input
+                                    type='number'
+                                    placeholder='Enter the Total Score'
+                                    name='total_score'
+                                    className='form-control'
+                                    value={student.total_score}
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <br />
+                            <div className='form-group'>
+                                <input
+                                    type='number'
+                                    placeholder='Enter Average CGPA'
+                                    name='avg_cgpa'
+                                    className='form-control'
+                                    value={student.avg_cgpa}
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <br />
+                            <div className="button-group">
+                                <button type="submit" className="btn btn-add">Submit</button>
+                                <button type="button" className="btn btn-cancel" onClick={() => navigate('/')}>Cancel</button>
+                            </div>
+                        </form>
                     </div>
-                    <br />
-                    <div className='form-group'>
-                        <input
-                            type='date'
-                            placeholder='Enter date of birth'
-                            name='dob'
-                            className='form-control'
-                            value={student.dob}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <br />
-                    <div className='form-group'>
-                        <input
-                            type='text'
-                            placeholder='Enter your current address'
-                            name='address'
-                            className='form-control'
-                            value={student.address}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <br />
-                    <div className='form-group'>
-                        <input
-                            type='text'
-                            placeholder='Enter True or False'
-                            name='attendence'
-                            className='form-control'
-                            value={student.attendence}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <br />
-                    <div className='form-group'>
-                        <input
-                            type='number'
-                            placeholder='Enter the Total Score'
-                            name='score'
-                            className='form-control'
-                            value={student.score}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <br />
-                    <div className='form-group'>
-                        <input
-                            type='number'
-                            placeholder='Enter Average CGPA'
-                            name='cgpa'
-                            className='form-control'
-                            value={student.cgpa}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <br />
-                    <input
-                type='submit'
-                className='btn btn-outline-warning btn-block mt-4'
-              />
-                </form>    
+                </div>
             </div>
         </div>
-    </div>
-</div>
 
     )
 }
